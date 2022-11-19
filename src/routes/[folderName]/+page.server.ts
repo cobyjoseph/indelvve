a72e226bd579aeccf582d408e66840a5eb7aa580
Coby Joseph <coby.joseph@gmail.com>
2022-11-18 21:58:23 -0500

    working on dgrpah

--- src/routes/[folderName]/+page.server.ts
+++ src/routes/[folderName]/+page.server.ts
@@ -2,29 +2,147 @@ import type { Actions } from './$types';
 import { SECRET_API_URL, SECRET_X_AUTH } from '$env/static/private';
 import { v4 as uuid } from 'uuid';
 import type { RequestHandler } from '@sveltejs/kit';
+import type { PageServerLoad } from './$types';
+import dgraph from 'dgraph-js';
 
-export const GET: RequestHandler = async () => {
-	async function fetchDQL(dqlQuery, operationName, variables) {
-		const result = await fetch('http://localhost:8080', {
-			method: 'POST',
-			headers: {},
-			body: JSON.stringify({
-				query: dqlQuery,
-				variables: variables,
-				operationName: operationName
-			})
-		});
-		return await result.json;
+
+// Create a client stub.
+function newClientStub() {
+	return new dgraph.DgraphClientStub('http://localhost:8080');
+}
+
+// Create a client.
+function newClient(clientStub) {
+	return new dgraph.DgraphClient(clientStub);
+}
+
+// Drop All - discard all data and start from a clean slate.
+async function dropAll(dgraphClient) {
+	await dgraphClient.alter({ dropAll: true });
+}
+
+// Set schema.
+async function setSchema(dgraphClient) {
+	const schema = `
+        name: string @index(exact) .
+        age: int .
+        married: bool .
+        loc: geo .
+        dob: datetime .
+    `;
+	await dgraphClient.alter({ schema: schema });
+}
+
+// // Create data using JSON.
+// async function createData(dgraphClient) {
+// 	// Create a new transaction.
+// 	const txn = dgraphClient.newTxn();
+// 	try {
+// 		// Create data.
+// 		const p = {
+// 			name: 'Alice',
+// 			age: 26,
+// 			married: true,
+// 			loc: {
+// 				type: 'Point',
+// 				coordinates: [1.1, 2]
+// 			},
+// 			dob: new Date(1980, 1, 1, 23, 0, 0, 0),
+// 			friend: [
+// 				{
+// 					name: 'Bob',
+// 					age: 24
+// 				},
+// 				{
+// 					name: 'Charlie',
+// 					age: 29
+// 				}
+// 			],
+// 			school: [
+// 				{
+// 					name: 'Crown Public School'
+// 				}
+// 			]
+// 		};
+
+// 		// Run mutation.
+// 		const assigned = await txn.mutate({ setJson: p });
+
+// 		// Commit transaction.
+// 		await txn.commit();
+
+// 		// Get uid of the outermost object (person named "Alice").
+// 		// Assigned#getUidsMap() returns a map from blank node names to uids.
+// 		// For a json mutation, blank node names "blank-0", "blank-1", ... are used
+// 		// for all the created nodes.
+// 		console.log(`Created person named "Alice" with uid = ${assigned.data.uids['blank-0']}\n`);
+
+// 		console.log('All created nodes (map from blank node names to uids):');
+// 		Object.keys(assigned.data.uids).forEach((key) =>
+// 			console.log(`${key} => ${assigned.data.uids[key]}`)
+// 		);
+// 		console.log();
+// 	} finally {
+// 		// Clean up. Calling this after txn.commit() is a no-op
+// 		// and hence safe.
+// 		await txn.discard();
+// 	}
+// }
+
+// Query for data.
+async function queryData(dgraphClient) {
+	// Run query.
+	const query = `query all($a: string) {
+        all(func: eq(name, $a)) {
+            uid
+            name
+            age
+            married
+            loc
+            dob
+            friend {
+                name
+                age
+            }
+            school {
+                name
+            }
+        }
+    }`;
+	const vars = { $a: 'Alice' };
+	const res = await dgraphClient.newTxn().queryWithVars(query, vars);
+	const ppl = res.data;
+
+	// Print results.
+	console.log(`Number of people named "Alice": ${ppl.all.length}`);
+	ppl.all.forEach((person) => console.log(person));
+}
+
+async function main() {
+	const dgraphClientStub = newClientStub();
+	const dgraphClient = newClient(dgraphClientStub);
+	await dropAll(dgraphClient);
+	await setSchema(dgraphClient);
+	await createData(dgraphClient);
+	await queryData(dgraphClient);
+}
+
+main()
+	.then(() => {
+		console.log('\nDONE!');
+	})
+	.catch((e) => {
+		console.log('ERROR: ', e);
+	});
+
+
+
+export const load: PageServerLoad = async () => {
+	function newClientStub() {
+		return new dgraph.DgraphClientStub('http://localhost:8080');
 	}
 
-	const dqlQuery = `
-		{
-			fetchTest(func: eq(name, "Coby1")){
-				uid
-				name
-				age
-		}
-		}`;
+	return {};
 };
 
 //// FOR DGRAPH CLOUD ONLY BELOW -----------------------------------------------------------
--- src/routes/api/getPosts/+server.ts
+++ src/routes/api/getPosts/+server.ts
@@ -2,6 +2,54 @@ import { SECRET_API_URL, SECRET_X_AUTH } from '$env/static/private';
 import type { RequestHandler } from '@sveltejs/kit';
 import fetch from 'node-fetch';
 
+// export const GET: RequestHandler = async () => {
+// 	async function fetchDql(dqlQuery, operationName, variables) {
+// 		const resultDql = await fetch('http://localhost:8080', {
+// 			method: 'POST',
+// 			headers: {},
+// 			body: JSON.stringify({
+// 				query: dqlQuery,
+// 				variables: variables,
+// 				operationName: operationName
+// 			})
+// 		});
+// 		return await resultDql.json;
+// 	}
+
+// 	const dqlQuery = `
+// 		{
+// 			fetchTest(func: eq(name, "Coby1")){
+// 				uid
+// 				name
+// 				age
+// 		}
+// 	}
+// 	`;
+
+// 	function fetchDqlQuery() {
+// 		return fetchDql(dqlQuery, null, { slug });
+// 	}
+
+// 	//give UUIDs to the data
+
+// 	const resultDql = await fetchDqlQuery()
+// 		.then(({ data, errors }) => {
+// 			if (errors) {
+// 				// handle those errors like a pro
+// 				console.error(errors);
+// 			}
+// 			return data;
+// 			console.log(data);
+// 		})
+// 		.catch((error) => {
+// 			// handle errors from fetch itself
+// 			console.error(error);
+// 		});
+
+// 	return new Response(JSON.stringify(resultDql));
+// };
+//------------------------------------------------------------------------------
+
 export const GET: RequestHandler = async ({ url }) => {
 	const slug = url.searchParams.get('slug');
 	console.log('slug:', slug);
@@ -44,7 +92,6 @@ export const GET: RequestHandler = async ({ url }) => {
 	  }
 	}
   `;
-
 
 	function fetchMyQuery() {
 		console.log('from fetchMyQuery:', slug);
