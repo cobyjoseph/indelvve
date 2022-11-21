7860a063a48cf03bb37e331b2459aa4ef6971565
Coby Joseph <coby.joseph@gmail.com>
2022-11-21 15:07:46 -0500

    mutating dgraph data works

--- src/routes/api/dgraph/+server.ts
+++ src/routes/api/dgraph/+server.ts
@@ -4,7 +4,9 @@ import type { RequestHandler } from '@sveltejs/kit';
 // // const dgraph = require('dgraph-js');
 // // const grpc = require('grpc');
 
-let res;
+//MAYBE I ACTUALLY NEED FETCHES ON BOTH SIDES.
+// ONE FETCH TO GET THE DATA FROM DGRAPH TO THE SERVER
+// ANOTHER FETCH TO GET IT FROM SERVER.TS TO +PAGE.TS - EG FROM THE SERVER TO BE ACCESSIBLE BY THE CLIENT
 
 export const GET: RequestHandler = async () => {
 	// Create a client stub.
@@ -17,74 +19,6 @@ export const GET: RequestHandler = async () => {
 		return new dgraph.DgraphClient(clientStub);
 	}
 
-	// Set schema.
-	// async function setSchema(dgraphClient) {
-	// 	const schema = `
-	//         name: string @index(exact) .
-	//         age: int .
-	//         married: bool .
-	//         loc: geo .
-	//         dob: datetime .
-	//     `;
-	// 	await dgraphClient.alter({ schema: schema });
-	// }
-
-	// Create data using JSON.
-	// async function createData(dgraphClient) {
-	// 	// Create a new transaction.
-	// 	const txn = dgraphClient.newTxn();
-	// 	try {
-	// 		// Create data.
-	// 		const p = {
-	// 			name: 'Alice',
-	// 			age: 26,
-	// 			married: true,
-	// 			loc: {
-	// 				type: 'Point',
-	// 				coordinates: [1.1, 2]
-	// 			},
-	// 			dob: new Date(1980, 1, 1, 23, 0, 0, 0),
-	// 			friend: [
-	// 				{
-	// 					name: 'Bob',
-	// 					age: 24
-	// 				},
-	// 				{
-	// 					name: 'Charlie',
-	// 					age: 29
-	// 				}
-	// 			],
-	// 			school: [
-	// 				{
-	// 					name: 'Crown Public School'
-	// 				}
-	// 			]
-	// 		};
-
-	// 		// Run mutation.
-	// 		const assigned = await txn.mutate({ setJson: p });
-
-	// 		// Commit transaction.
-	// 		await txn.commit();
-
-	// 		// Get uid of the outermost object (person named "Alice").
-	// 		// Assigned#getUidsMap() returns a map from blank node names to uids.
-	// 		// For a json mutation, blank node names "blank-0", "blank-1", ... are used
-	// 		// for all the created nodes.
-	// 		console.log(`Created person named "Alice" with uid = ${assigned.data.uids['blank-0']}\n`);
-
-	// 		console.log('All created nodes (map from blank node names to uids):');
-	// 		Object.keys(assigned.data.uids).forEach((key) =>
-	// 			console.log(`${key} => ${assigned.data.uids[key]}`)
-	// 		);
-	// 		console.log();
-	// 	} finally {
-	// 		// Clean up. Calling this after txn.commit() is a no-op
-	// 		// and hence safe.
-	// 		await txn.discard();
-	// 	}
-	// }
-
 	// Query for data.
 	async function queryData(dgraphClient) {
 		// Run query.
@@ -95,15 +29,65 @@ export const GET: RequestHandler = async () => {
 			age
 	  }
 	}`;
-		// const vars = { $a: 'Alice' };
-		const res = await dgraphClient.newTxn().query(query);
-		// const dataPoint = res.data;
-		// const ppl = res.data;
 
-		// Print results.
-		// console.log(`Number of people named "Alice": ${ppl.all.length}`);
-		// ppl.all.forEach((person) => console.log(person));
+		const res = await dgraphClient.newTxn().query(query);
+
 		return { res };
+	}
+
+	async function createData(dgraphClient) {
+		// Create a new transaction.
+		const txn = dgraphClient.newTxn();
+		try {
+			// Create data.
+			const p = {
+				name: 'Alice',
+				age: 26,
+				married: true,
+				loc: {
+					type: 'Point',
+					coordinates: [1.1, 2]
+				},
+				dob: new Date(1980, 1, 1, 23, 0, 0, 0),
+				friend: [
+					{
+						name: 'Bob',
+						age: 24
+					},
+					{
+						name: 'Charlie',
+						age: 29
+					}
+				],
+				school: [
+					{
+						name: 'Crown Public School'
+					}
+				]
+			};
+
+			// Run mutation.
+			const assigned = await txn.mutate({ setJson: p });
+
+			// Commit transaction.
+			await txn.commit();
+
+			// Get uid of the outermost object (person named "Alice").
+			// Assigned#getUidsMap() returns a map from blank node names to uids.
+			// For a json mutation, blank node names "blank-0", "blank-1", ... are used
+			// for all the created nodes.
+			console.log(`Created person named "Alice" with uid = ${assigned.data.uids['blank-0']}\n`);
+
+			console.log('All created nodes (map from blank node names to uids):');
+			Object.keys(assigned.data.uids).forEach((key) =>
+				console.log(`${key} => ${assigned.data.uids[key]}`)
+			);
+			console.log();
+		} finally {
+			// Clean up. Calling this after txn.commit() is a no-op
+			// and hence safe.
+			await txn.discard();
+		}
 	}
 
 	async function main() {
@@ -111,7 +95,7 @@ export const GET: RequestHandler = async () => {
 		const dgraphClient = newClient(dgraphClientStub);
 		// await dropAll(dgraphClient);
 		// await setSchema(dgraphClient);
-		// await createData(dgraphClient);
+		await createData(dgraphClient);
 		await queryData(dgraphClient);
 	}
 
@@ -119,7 +103,6 @@ export const GET: RequestHandler = async () => {
 		.then(() => {
 			console.log('\nDONE!');
 			console.log('logging queryData', queryData);
-			return res;
 		})
 		.catch((e) => {
 			console.log('ERROR: ', e);
--- src/routes/testFolder/+page.ts
+++ src/routes/testFolder/+page.ts
@@ -1,9 +1,9 @@
-export async function load({ fetch, params }) {
+export async function load({ fetch }) {
 	// when you don't specify the fetch method, the default is a get request. that's what happens here. get request to our api
 	const res = await fetch(`/api/dgraph`);
 	const data = await res.json();
-    console.log('data from +page.ts', data)
-    console.log('res from +page.ts', res)
+	console.log('data from +page.ts', data);
+	console.log('res.body from +page.ts', res.body);
 
 	return {
 		data
